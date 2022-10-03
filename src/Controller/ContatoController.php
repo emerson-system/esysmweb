dca498315f38023ce3878a7eecd5c04fff20e52f
emersonsilva <emersonsilva@deso-se.com.br>
2022-10-03 16:09:37 -0300

    Correção do envio de msg no contato

--- src/Controller/ContatoController.php
+++ src/Controller/ContatoController.php
@@ -39,10 +39,10 @@ class ContatoController extends AppController
         if($this->request->is('post')){
             $contatoMsg = $contatoMsgsTable->patchEntity($contatoMsg, $this->request->getData());
             if($contatoMsgsTable->save($contatoMsg)){                
-                //$this->getMailer('Contato')->send('novaMsgContato',[$contatoMsg]);
-                //$contatoMsg->emailAdm = 'celkeadm@gmail.com';
-                //$this->getMailer('Contato')->send('novaMsgContatoAdm',[$contatoMsg]);
-                $this->Flash->success(__('Mensagem de contato enviada com sucesso. Para enviar o e-mail retire o comentário no arquivo ContatoController no método index a parte de enviar o email ($this->getMailer...)'));
+                $this->getMailer('Contato')->send('novaMsgContato',[$contatoMsg]);
+                $contatoMsg->emailAdm = 'sistemas@esymweb.com.br';
+                $this->getMailer('Contato')->send('novaMsgContatoAdm',[$contatoMsg]);
+                $this->Flash->success(__('Mensagem de contato enviada com sucesso.'));
                 return $this->redirect(['controller' => 'Contato', 'action' => 'index']);
             }else{
                 $this->Flash->danger(__('Erro: Mensagem de contato não foi enviada com sucesso. Preencha todos os campos!'));
